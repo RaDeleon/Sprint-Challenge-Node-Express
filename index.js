950cc670312c58db46646778846162bc41b71d0b
Angelo Deleon <rudy209@comcast.net>
2018-11-16 10:32:03 -0800

    MVP Complete

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const server = express();
+
 const projectDB = require("./data/helpers/projectModel");
 const actionDB = require("./data/helpers/actionModel");
 const projectRouter = require("./projects/projectRouter");
 const actionRouter = require("./actions/actionRouter");
-const port = 9000;
 
+const port = 9000;
+// const cors = require("cors");
 server.use(express.json());
+
+// R00t
+
 server.get("/", (req, res) => {
     res.status(200).send("<h1>Here's the <em>root directory</em></h1>");
 });
@@ -14,4 +19,5 @@ server.get("/", (req, res) => {
 
 server.use("/projects", projectRouter);
 server.use("/actions", actionRouter);
+
 server.listen(port, () => console.log(`Listening on port ${port}`));
